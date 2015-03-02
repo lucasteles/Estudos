381ccca01452d2355df9d51a590c23e0eb173b84
Lucas Teles <lucas.teles@outlook.com>
2015-03-02 11:45:25 -0300

    Basic PGM viwer

---
+++ CSharp/PGM_EDITOR/PGM_EDITOR/main.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PGM_EDITOR
+{
+    /// <summary>
+    /// Interaction logic for main.xaml
+    /// </summary>
+    public partial class main : Window
+    {
+        private Bitmap bitmap;
+        private Stack<Bitmap> history = new Stack<Bitmap>();
+        
+        
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Selecione uma foto";
+            op.Filter = "All supported graphics|*.pgm";
+
+            
+            
+
+            if (op.ShowDialog() == true)
+            {
+ 
+                bitmap = PGMUtil.ToBitmap(op.FileName);
+                show();
+            }
+        }
+
+
+        private void show()
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+             using (MemoryStream memory = new MemoryStream())
+                {
+                    bitmap.Save(memory, ImageFormat.Png);
+                    memory.Position = 0;
+                    bitmapImage.BeginInit();
+                    bitmapImage.StreamSource = memory;
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.EndInit();
+                }
+                
+                imgPhoto.Source = bitmapImage;
+        }
+
+        private void btnRotateR_Click(object sender, RoutedEventArgs e)
+        {
+            history.Push(bitmap.Clone() as Bitmap);
+            bitmap = tools.RotateR(bitmap);
+            show();
+        }
+        private void btnRotateL_Click(object sender, RoutedEventArgs e)
+        {
+            history.Push(bitmap.Clone() as Bitmap);
+            bitmap = tools.RotateL(bitmap);
+            show();
+        }
+
+        private void btnUndo_Click(object sender, RoutedEventArgs e)
+        {
+            if (history.Count == 0)
+                return;
+
+            bitmap = history.Pop();
+            show();
+        }
+
+    }
+}
---
+++ CSharp/PGM_EDITOR/PGM_EDITOR/tools.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using draw = System.Drawing;
+
+
+namespace PGM_EDITOR
+{
+    public static class tools
+    {
+        public static Color[,] Bitmap2ColorMatrix(Bitmap bitmapFile)
+        {
+
+            int hight = bitmapFile.Height;
+            int width = bitmapFile.Width;
+
+            Color[,] colorMatrix = new Color[width,hight];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < hight; j++)
+                {
+                    colorMatrix[i, j] = bitmapFile.GetPixel(i, j);
+                }
+            }
+            return colorMatrix;
+        }
+
+        public static Bitmap ColorMatrix2Bitmap(Color[,] matriz)
+        {
+            int width = matriz.GetLength(0);
+            int height = matriz.GetLength(1);
+            
+            var b1 = new Bitmap(width, height);
+
+           
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    b1.SetPixel(i, j, matriz[i, j]);
+                }
+
+            }
+                      
+
+            return b1;
+        }
+        
+        public static Bitmap RotateR(Bitmap bitmap)
+        {
+            var ret = new Bitmap(bitmap.Width, bitmap.Height);
+
+            var matriz = Bitmap2ColorMatrix(bitmap);
+
+
+            matriz = MirrorX(Transpose(matriz));
+
+            
+            ret = ColorMatrix2Bitmap(matriz);
+
+            return ret;
+        }
+
+        public static Bitmap RotateL(Bitmap bitmap)
+        {
+            var ret = new Bitmap(bitmap.Width, bitmap.Height);
+
+            var matriz = Bitmap2ColorMatrix(bitmap);
+
+
+            matriz = MirrorY(Transpose(matriz));
+
+
+            ret = ColorMatrix2Bitmap(matriz);
+
+            return ret;
+        }
+
+        public static Color[,] Transpose(Color[,] matrix)
+        {
+            Color[,] temp =new Color[matrix.GetLength(1),matrix.GetLength(0) ];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    temp[j, i] = matrix[i, j];
+                }
+            }
+
+            return temp;
+        }  
+
+        public static  Color[,] MirrorX(Color[,] matrix)
+        {
+            Color[,] temp = new Color[matrix.GetLength(0), matrix.GetLength(1)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    temp[i, j] = matrix[matrix.GetLength(0)-1-i, j];
+                }
+            }
+
+            return temp;
+
+        }
+
+        public static Color[,] MirrorY(Color[,] matrix)
+        {
+            Color[,] temp = new Color[matrix.GetLength(0), matrix.GetLength(1)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    temp[i, j] = matrix[i, matrix.GetLength(1) - 1 - j];
+                }
+            }
+
+            return temp;
+
+        }
+
+    }
+}
