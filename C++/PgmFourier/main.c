62a653bba2e141d3a508e3bc4cff4d4d8fe83a2e
Lucas Teles <lucas.teles@outlook.com>
2015-05-27 17:40:35 -0300

    acerto no fourrier

--- C++/PgmFourier/main.c
+++ C++/PgmFourier/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     PGMFile *file = (PGMFile*)malloc(sizeof(PGMFile));
 
-    //teste!
+
     // ira receber como parametro na executavel
     argv[1] = "baboon.pgm";
     argv[2] = "baboon2.pgm";
@@ -41,5 +41,63 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+void CalcFourier(PGMFile *file)
+{
+    int i,j;
+    int w = file->width;
+    int h = file->height;
+
+    file->imatrix = (INumber**)alloc_Imatrix(h,w);
+
+    for(i=0;i<h;i++)
+        file->imatrix[i] = Fourier(file->matrix[i],i,w,0);
+
+    file->espectro = alloc_matrix(h,w);
+
+    for(i=0;i<h;i++)
+        for(j=0;j<w;j++)
+            file->espectro[i][j] = Espectro(file->imatrix[i][j]);
+}
 
 
+INumber* Fourier(int *matriz,int l,int h,int inversa)
+{
+    int Neg = (inversa ? 1 : -1);
+    
+    int N = h;
+    int freq,t=0;
+    INumber *frequencies = malloc(sizeof(INumber) * N);
+
+    for (freq = 0; freq < N; freq++) {
+        double re = 0;
+        double im = 0;
+
+        for (; t < N; t++) {
+            double time= (double)t;
+            double var = matriz[t];
+
+            double rate = Neg * (2.0 * PI) * freq * time / N;
+
+            double re_part = var * cos(rate);
+            double im_part = var * sin(rate);
+
+            re += re_part;
+            im += im_part;
+        }
+
+        if(inversa){
+            re = re / N;
+            im = im / N;
+        }
+
+        frequencies[freq].r = re;
+        frequencies[freq].i = im;
+    }
+
+    return frequencies;
+}
+
+
+int Espec(INumber num){
+    return (int)log(1 + sqrt(num.r*num.r + num.i*num.i));
+}
