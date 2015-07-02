ec11722b80c1e70697f68c62a3b6b8c4fbd56e3f
Lucas Teles <lucas.teles@outlook.com>
2015-07-01 21:05:11 -0300

    site teste de json

---
+++ CSharp/JsonParserToCSharp/JsonToCSharp/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using JsonParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsonToCSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        // GET: Home
+        public ContentResult Index()
+        {
+            return Content("error.");
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public JsonResult GetParse(string txtJson)
+        {
+            //ViewBag.Value = txtJson;
+            var code = "";
+            if (txtJson!=null && !txtJson.Equals(""))
+            {
+                using (var fileStream = GenerateStreamFromString(txtJson))
+                using (var binaryReader = new BinaryReader(fileStream))
+                {
+                    var machine = new JsonMachine(binaryReader);
+                    string err;
+                    if (!machine.Analisys(out err))
+                    { 
+                       code = "Error: " + err;
+                    }
+                    else
+                    {
+                        code = machine.GetCode();
+                    }
+                }
+            }
+            else { code =  ""; }
+
+            return Json(new { parse = code });//View("Index");
+        }
+
+       
+
+        public Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+    }
+}
---
+++ CSharp/JsonParserToCSharp/JsonToCSharp/obj/Release/Package/PackageTmp/Parser/js/site.js
@@ -0,0 +1,58 @@
+$(function(){
+	$('.loader').hide();
+    $('.social-anchor').hover(function () {
+        TweenMax.to($(this).find('.social'), 1, { rotationY: '+=360', ease: Linear.easeNone, repeat: -1 });
+    }, function () {
+        TweenMax.to($(this).find('.social'), 0.5, { rotationY: '0', ease: Linear.easeNone });
+    });
+
+    $('pre code').each(function (i, block) {
+        hljs.highlightBlock(block);
+    });
+
+
+
+
+    $('#btnSend').click(function(){
+
+    	 var ddata = new FormData();
+    	 ddata.append( 'txtJson', $('#txtJson').val());
+
+    	$.ajax({
+		  type: "POST",
+		  processData: false,
+  		  contentType: false,
+		  url: "../Home/GetParse",
+		  data: ddata,  
+		  crossDomain: true,
+		  success: function(data){
+		      $("#theCode").text(data.parse);
+		      ZeroClipboard.setData("text/plain", $("#theCode").text());
+		  	   $('pre code').each(function (i, block) {
+			        hljs.highlightBlock(block);
+			    });
+		  },
+		  error: function(data){
+		  	//ee = (data);
+		  },
+		  beforeSend: function(){
+		     $('.loader').fadeIn();
+		   },
+		   complete: function(){
+		     $('.loader').fadeOut();
+		   }
+		  
+		});    	
+
+    });
+
+    ZeroClipboard.setData("text/plain", $("#theCode").text());
+    var client = new ZeroClipboard(document.getElementById("copy-button"));
+       client.on("ready", function (readyEvent) {
+        client.on("aftercopy", function (event) {
+            event.target.style.display = "none";
+            $("#copy-button").show();
+            alert("Copied!");
+        });
+    });
+});
