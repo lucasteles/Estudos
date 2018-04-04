3c8b7995115da99fc44627f25c8f41d34c627df6
Lucas Teles <lucas.teles@lambda3.com.br>
2018-04-04 15:30:02 -0300

    estudos threads

---
+++ CSharp/Semaphore/Semaphore/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace PartyHard
+{
+    class Program
+    {
+        static Semaphore semaphore = new Semaphore(3, 3);
+        static Thread[] threads = new Thread[10];
+
+
+        static int[] numeros = Enumerable.Range(1, 10).ToArray();
+
+        static void PartyHard()
+        {
+
+
+            Console.WriteLine(
+                $"{Thread.CurrentThread.Name} is waiting in line...");
+
+            semaphore.WaitOne();
+            Console.WriteLine(
+                $"{Thread.CurrentThread.Name} enters the party!");
+
+            Thread.Sleep(300);
+            Console.WriteLine(
+                $"{Thread.CurrentThread.Name} is leaving the party");
+            semaphore.Release();
+        }
+
+        static void Main()
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    threads[i] = new Thread(PartyHard);
+            //    threads[i].Name = "thread_" + i;
+            //    threads[i].Start();
+            //}
+
+            Console.Error.WriteLine("deu ruim");
+            Console.WriteLine("so que nao");
+
+            var lst = new[] { 1, 2, 3, 5, 6, 7 };
+            var l1 = lst.Shuffle().ToArray();
+            var l2 = lst.Shuffle().ToArray();
+            var l3 = lst.Shuffle().ToArray();
+
+
+            var z = new StringBuilder();
+            var teste = new TESTE(z);
+            var x = Marshal.SizeOf(teste);
+
+
+            numeros.ToList().ForEach(Console.WriteLine);
+            ref readonly var numero = ref GetRefValur(numeros, 5);
+            ref readonly var numero2 = ref GetRefValur(numeros, 1);
+
+            numeros[1] = -999;
+            numeros[5] = -666;
+
+
+            Console.WriteLine($"{numero} e {numero2}");
+
+            //IntPtr ptr;
+            //int a = 1;
+            //string b = "fdsasd";
+
+
+            //unsafe
+            //{
+            //    double[] arr = { 0, 1.5, 2.3, 3.4, 4.0, 5.9 };
+            //    int* dado = stackalloc int[10];
+            //    int* dadoHealp = (int*)Marshal.AllocHGlobal(10 * sizeof(int));
+            //    ptr = (IntPtr)dadoHealp;
+
+            //    fixed (double* p = &arr[0])
+            //    {
+            //        *p = 1;
+            //    }
+            //    Marshal.FreeHGlobal(ptr);
+
+            //}
+
+
+            if (
+                MetodoComNomeMuitoGrandeParaEscreverVaiFilhao(2) is var n &&
+                n > 0 && n < 10
+               )
+            {
+                Console.WriteLine(n);
+            }
+
+
+
+            Console.Read();
+
+            //numero = 10;
+        }
+
+
+        static ref readonly int GetRefValur(int[] numeros, int index)
+        {
+            return ref numeros[index];
+        }
+
+        static int MetodoComNomeMuitoGrandeParaEscreverVaiFilhao(int n) => n * 2;
+
+        readonly struct TESTE
+        {
+            readonly StringBuilder Nome;
+            // readonly int Idade;
+            public TESTE(StringBuilder nome)
+            {
+                Nome = nome;
+                //Idade = idade;
+            }
+        }
+
+    }
+
+    public static class ext
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> list) => list.Shuffle(new Random());
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> list, Random r) =>
+                list
+                   .Select(x => new { Number = r.Next(), Item = x })
+                   .Do(e => Console.WriteLine(e.Item))
+                   .OrderBy(x => x.Number)
+                   .Select(x => x.Item);
+    }
+}
---
+++ CSharp/TestParallel/TestParallel/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestParallel
+{
+    class Program
+    {
+        const int SLEEP = 1000;
+
+        static async Task Main(string[] args)
+        {
+
+            if (Teste(5) is var x && Comparar(x.Value, 5))
+            {
+                Console.WriteLine(x);
+            }
+
+            if (Teste(20) is var y && Comparar(y.Value, 20))
+            {
+                Console.WriteLine(y);
+            }
+
+
+            int a = 2147483647;
+            int b = 2147483647;
+            int value = a + b;
+
+            long a2 = 2147483647;
+            long b2 = 2147483647;
+            long v2 = a2 + b2;
+
+            int v3 = (int)v2;
+            //        int v4 = checked((int)a2);
+            //      int v5 = Convert.ToInt32(v2);
+
+            Enumerable.Range(0, 20)
+                .Select(e => new { A = e, B = (char)e })
+                .ToLookup(e => new { e.A, e.B })
+                .ToList()
+                ;
+
+
+
+
+            var numbers = Enumerable.Range(1, 100);
+
+            var r = new Stopwatch();
+            //r.Start();
+            //foreach (var item in numbers)
+            //{
+            //    Slow(item);
+            //}
+            //r.Stop();
+            //var seqTime = r.ElapsedMilliseconds;
+            var seqTime = SLEEP * numbers.Count();
+
+            r.Restart();
+            numbers
+              .AsParallel()
+              .WithDegreeOfParallelism(Environment.ProcessorCount)
+              .WithMergeOptions(ParallelMergeOptions.NotBuffered)
+              .ForAll(e => Slow(e));
+
+            r.Stop();
+            var t1 = r.ElapsedMilliseconds;
+            Console.WriteLine("-----------");
+            r.Restart();
+            await numbers.ToObservable()
+               .SubscribeOn(ThreadPoolScheduler.Instance)
+               .Select(e => Observable.Start(() => Slow(e)))
+               .Merge(Environment.ProcessorCount);
+
+            r.Stop();
+
+            var t2 = r.ElapsedMilliseconds;
+
+
+
+            Console.WriteLine($"Sequencial = {seqTime}");
+            Console.WriteLine($"PLINQ = {t1} | diffSeq = {seqTime - t1}");
+            Console.WriteLine($"RX = {t2} | diffSeq = {seqTime - t2}");
+
+
+            Console.WriteLine($"diff PLINQ-RX {t1 - t2}");
+
+            Console.ReadKey();
+        }
+
+        public static int? Teste(int x) => x > 10 ? null : (int?)x;
+        public static bool Comparar(int a, int b)
+        {
+            Console.WriteLine($"comparando {a} r {b}");
+            return a == b;
+        }
+
+        public static int Slow(int n)
+        {
+            Thread.Sleep(SLEEP);
+            Console.WriteLine($"{n} on thread {Thread.CurrentThread.ManagedThreadId}");
+            return n;
+        }
+
+
+
+    }
+}
