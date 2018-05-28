6e9bcff264b7a4498d496eb654d03afdd56b468d
Lucas Teles <eu@lucasteles.net>
2018-05-28 20:29:53 -0300

    elmish xamarin with xaml

---
+++ FSharp/Elmish.CounterApp/CounterApp.Droid/MainActivity.fs
@@ -0,0 +1,23 @@
+﻿namespace CounterApp.Droid
+
+open System
+
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Xamarin.Forms.Platform.Android
+type Resources = CounterApp.Droid.Resource
+
+[<Activity (Label = "CounterApp.Droid", MainLauncher = true, Icon = "@mipmap/icon")>]
+type MainActivity() =
+    inherit FormsApplicationActivity()
+    override this.OnCreate (bundle) =
+        base.OnCreate (bundle)
+
+        Xamarin.Forms.Forms.Init (this, bundle)
+
+        this.LoadApplication (new CounterApp.CounterApp ())
+
---
+++ FSharp/Elmish.CounterAppXaml/CounterApp/CounterPage.xaml.fs
@@ -0,0 +1,9 @@
+﻿namespace CounterApp
+
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+type CounterPage() =
+    inherit ContentPage()
+    let _ = base.LoadFromXaml(typeof<CounterPage>)
+
