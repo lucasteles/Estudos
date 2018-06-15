381b9d6159d8c62220359acf8a01dfce29f52fae
Lucas Teles <eu@lucasteles.net>
2018-06-15 18:28:17 -0300

    novo jogo da velha

--- Haskell/velha.hs
+++ Haskell/velha.hs
@@ -1,115 +1,74 @@
-jogovelha = do
-    let tab = ["123","456","789"]
-    do iniciar 'P' tab "123456789"
 
-iniciar  p tab jogo = do
-    if (p =='P') 
-    then (putStrLn "\n\n* PRETO * \n") 
-    else (putStrLn "\n\n* BRANCO * \n") 
+import Data.List
+import Data.List.Split
+import Control.Monad.State
 
-    putStrLn ( get_line tab 1)    
-    putStrLn ( get_line tab 2)  
-    putStrLn ( get_line tab 3)  
+type Posicao = Int 
+data Jogador = Bola | Cruz 
 
-    j <- validarjogada jogo
-    
-    let jogo_n  = output j jogo
-    let jn      = (read [j] :: Int) 
-    let tab_n   = (jogada tab p jn)
-    venc <- vencedor tab_n p
+data Quadro = Quadro (Maybe Jogador) Posicao
+type Tabuleiro = [Quadro]
 
-    if (isempty jogo_n || venc == 'P' || venc == 'B') then do{
-        putStrLn ( "\n");
-        putStrLn ( get_line tab_n 1);
-        putStrLn ( get_line tab_n 2); 
-        putStrLn ( get_line tab_n 3); 
-        if(venc == 'V') 
-        then (putStr "\n\nEmpate!")
-        else do{
-            putStr "\n\nJogador ";
-            putStr ([venc]);
-            putStr " Venceu!";
-        }
-    }
-    else
-        if (p == 'P') then (iniciar 'B' tab_n jogo_n ) else (iniciar 'P' tab_n jogo_n)
+data Jogada = Jogada Posicao deriving Show
 
-validarjogada jogo = do
-    putStrLn ("\nPosicao 1 ate 9")
-    j <- getChar
-    if (isin j jogo)
-    then (return(j))
-    else do{
-    putStrLn ("\nPosicao errada!\n");
-    do validarjogada jogo;
-    }
+type EstadoDoJogo = State (Jogador, Tabuleiro) (Jogador, Tabuleiro) 
 
-isempty :: [Char] -> Bool
-isempty [] = True
-isempty _  = False
+instance Show Jogador where
+    show Bola = "O"
+    show Cruz = "X"
 
-isin :: Char -> [Char] -> Bool
-isin _ [] = False
-isin p (x:xs) = if (x == p) then (True) else (isin p xs)
+instance Show Quadro where
+    show (Quadro Nothing a) = show (a+1)
+    show (Quadro (Just j) _) = show j
 
-output :: Char -> [Char] -> [Char]
-output p (x:xs) = if (x == p) then (xs) else ([x] ++ output p xs)
+exibirTabuleiro :: Tabuleiro -> String
+exibirTabuleiro = concat 
+                    . intersperse "\n" 
+                    . map (unwords . intersperse "|" . map show) . chunksOf 3
 
-get_line :: [String] -> Integer -> String
-get_line (x:xs) n = tab (tab_line (x:xs) n) n
-        
-tab_line :: [String] -> Integer -> String
-tab_line (x:xs) 1 = x  
-tab_line (x:xs) n = (tab_line xs (n-1)) 
-        
-tab :: [Char] -> Integer -> [Char]
-tab (x:xs) 1 = "   | ----- | ----- | ----- |\n"++linha(x:xs)
-tab (x:xs) 3 = linha(x:xs)++"\n   | ----- | ----- | ----- |"
-tab (x:xs) 2 = linha(x:xs)
+replaceNth n newVal (x:xs)
+     | n == 0 = newVal:xs
+     | otherwise = x:replaceNth (n-1) newVal xs
 
-linha :: [Char] -> [Char]
-linha [] = "   |"
-linha (x:xs) = "   |   " ++ (x:[]) ++ linha xs
+criarTabuleiro = map (Quadro Nothing) [0..8]
 
-marca :: [Char] -> Int -> Char -> [Char]
-marca (x:xs) 0 p = [p] ++ xs
-marca (x:xs) n p = x : marca xs (n-1) p
-    
-jogada :: [String] -> Char -> Int -> [String]
-jogada [] _ _ =  []
-jogada (x:xs) p n = (if ( n<=3 && n>0) then marca x (n-1) p else x) : (jogada xs p (n-3)) 
+proximoJogador Bola = Cruz
+proximoJogador Cruz = Bola
 
-vencedor list v = do
-    let v1 = vencedor1 list v 
-    let v2 = vencedor2 list v 1
-    let v3 = if((count (outc (outs list 1) 1 ++ outc (outs list 2) 2 ++ outc (outs list 3) 3) v)==3) then (v) else ('V')
-    let v4 = if((count (outc (outs list 1) 3 ++ outc (outs list 2) 2 ++ outc (outs list 3) 1) v)==3) then (v) else ('V')
-    
-    if(v1 == v || v2 == v || v3 == v || v4 == v) then (return(v)) else (return('V'))
+marcar :: Tabuleiro -> Jogada -> Jogador -> Tabuleiro
+marcar tab (Jogada index) jogador = replaceNth (index-1) (Quadro (Just jogador) index) tab
 
-vencedor1 :: [String] -> Char -> Char
-vencedor1 [] _     = 'V'
-vencedor1 (x:xs) v = if((count x v) == 3) then (v) else (vencedor1 xs v)
+realizarJogada :: Jogada -> EstadoDoJogo
+realizarJogada (Jogada onde) = do
+    (jogador, tab) <- get
+    let nTab = marcar tab (Jogada onde) jogador
+    let proximo = proximoJogador jogador
+    return (proximo, nTab)
 
-vencedor2 :: [String] -> Char ->  Int -> Char
-vencedor2 _ _ 4 = 'V'
-vencedor2 (x:xs) v c = if((count (outc (outs (x:xs) 1) c ++ outc (outs (x:xs) 2) c ++ outc (outs (x:xs) 3) c) v)==3) then (v) else (vencedor2 (x:xs) v (c+1))
+podeJogar :: Jogada -> Tabuleiro -> Bool
+podeJogar (Jogada onde) tab = case tab !! (onde-1) of
+                                 Quadro (Just _)  _-> False
+                                 Quadro (Nothing) _ -> True
 
-outs :: [String] -> Int -> [Char]
-outs (x:[]) 1 = x
-outs (x:xs) 1 = x
-outs (x:xs) c = outs xs (c-1)
+main = do
+    putStrLn "Bem vindo ao jogo da velha"
+    jogar (Cruz, criarTabuleiro)
 
-outc :: [Char] -> Int -> [Char]
-outc [] _ = []
-outc (x:[]) 1 = [x]
-outc (x:xs) 1 = [x]
-outc (x:xs) c = outc xs (c-1)
+jogar state@(jogador, tab) = do
+    putStrLn $ exibirTabuleiro  tab
+    putStrLn $ "Vez do " ++ show jogador ++ ":"
+    ondeStr <- getLine 
 
-count :: [Char] -> Char -> Int
-count [] _ = 0
-count (x:xs) c = if(x==c) then ( 1 + count xs c) else (count xs c)
+    let onde = Jogada $ read ondeStr
+    let jogadaValida = podeJogar onde tab
 
-tochar :: [Char] -> Char
-tochar [] = ' '
-tochar (x:xs) = x
+    if not jogadaValida then do 
+        putStrLn "Jogada invalida"
+        jogar state
+    else do
+        let jogada = realizarJogada onde 
+        let novoState = evalState jogada state
+        jogar novoState
+
+    return ()
+
